be901d46e38f16093af5d428c6f2b52f5786b142
Greg Dunn <greg@vitrue.com>
2012-05-08 11:42:00 -0400

    added test state, doubled the spaces available in each measure (16th notes)

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.2'
-gem 'pg'
+gem 'sqlite3'
 gem 'rails-backbone', '~> 0.7.0'
 gem 'jquery-rails'
 gem 'ejs'
--- db/fixtures/states.rb
+++ db/fixtures/states.rb
@@ -1,5 +1,6 @@
 State.seed(:id,
 	{ :id => 1, :name => "New"  },
 	{ :id => 2, :name => "In Progress"  },
-	{ :id => 3, :name => "Published"  }
+	{ :id => 3, :name => "Published"  },
+	{ :id => 4, :name => "Test"}
 )
